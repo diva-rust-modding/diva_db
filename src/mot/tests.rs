3208ead39f0c37b71a205556c0a58f736ca0efb7
waelwindows <waelwindows9922@gmail.com>
2023-01-18 23:33:33 +0300

    fix(mot/test): Use updated function signature

--- src/mot/tests.rs
+++ src/mot/tests.rs
@@ -4,11 +4,15 @@ const INPUT: &[u8] = include_bytes!("../../assets/aft_mot_db.bin");
 
 #[test]
 fn read_test() {
-    let (_, mot_db) = MotionSetDatabase::read(Endianness::Little)(INPUT).unwrap();
+    let (_, mot_db) = MotionSetDatabase::read(INPUT).unwrap();
     println!("{:#?}", mot_db);
     println!("{:#?}", mot_db.bones.len());
     //find all ude
-    let udes = mot_db.bones.iter().enumerate().filter(|(_, n)| n.contains("ude"));
+    let udes = mot_db
+        .bones
+        .iter()
+        .enumerate()
+        .filter(|(_, n)| n.contains("ude"));
     for (i, ude) in udes {
         println!("{}: {}", i, ude);
     }
