35d26007fe83e45dcc3016671c07a86737dc8fe7
waelwindows <waelwindows9922@gmail.com>
2023-01-23 22:07:34 +0300

    feat(aet): Implement common traits

--- src/aet/mod.rs
+++ src/aet/mod.rs
@@ -5,27 +5,27 @@ use std::collections::BTreeMap;
 
 mod io;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDb {
     pub sets: BTreeMap<u32, AetDbSet>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDbSet {
+    #[cfg_attr(feature = "serde", serde(skip))]
+    index: i32,
     pub name: String,
     pub filename: String,
     pub spr_set_id: u32,
     pub scenes: BTreeMap<u32, AetDbScene>,
-    #[cfg_attr(feature = "serde", serde(skip))]
-    index: i32,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDbScene {
-    pub name: String,
     #[cfg_attr(feature = "serde", serde(skip))]
     index: u16,
+    pub name: String,
 }
