50eac256ddfe6df53257de6ea0da396822231ee0
waelwindows <waelwindows9922@gmail.com>
2023-01-23 21:19:52 +0300

    feat(spr): Add `serde` support

--- src/spr/mod.rs
+++ src/spr/mod.rs
@@ -1,14 +1,20 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
 use std::collections::BTreeMap;
 
 mod io;
 
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct SprDb {
     pub sets: BTreeMap<u32, SprDbSet>,
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct SprDbSet {
+    #[cfg_attr(feature = "serde", serde(skip))]
     index: i32,
     pub name: String,
     pub filename: String,
@@ -17,7 +23,9 @@ pub struct SprDbSet {
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct SprDbEntry {
+    #[cfg_attr(feature = "serde", serde(skip))]
     index: u16,
     pub name: String,
 }
