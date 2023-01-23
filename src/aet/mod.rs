675b872102c8cd41b387ed8de1eb6e8aaee6f891
waelwindows <waelwindows9922@gmail.com>
2023-01-23 22:06:54 +0300

    feat(aet): Implement serde

--- src/aet/mod.rs
+++ src/aet/mod.rs
@@ -1,25 +1,31 @@
-use std::collections::BTreeMap;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
 
-use crate::spr;
+use std::collections::BTreeMap;
 
 mod io;
 
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDb {
     pub sets: BTreeMap<u32, AetDbSet>,
 }
 
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDbSet {
     pub name: String,
     pub filename: String,
     pub spr_set_id: u32,
     pub scenes: BTreeMap<u32, AetDbScene>,
+    #[cfg_attr(feature = "serde", serde(skip))]
     index: i32,
 }
 
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct AetDbScene {
     pub name: String,
+    #[cfg_attr(feature = "serde", serde(skip))]
     index: u16,
 }
