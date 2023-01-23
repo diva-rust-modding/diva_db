e37e0c29b8fd94afe417076e42003c036a932c5e
waelwindows <waelwindows9922@gmail.com>
2023-01-23 20:40:49 +0300

    feat(spr): Implement common traits for structs

--- src/spr/mod.rs
+++ src/spr/mod.rs
@@ -2,22 +2,22 @@ use std::collections::BTreeMap;
 
 mod io;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
 pub struct SprDb {
     pub sets: BTreeMap<u32, SprDbSet>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 pub struct SprDbSet {
+    index: i32,
     pub name: String,
     pub filename: String,
     pub sprites: BTreeMap<u32, SprDbEntry>,
     pub textures: BTreeMap<u32, SprDbEntry>,
-    index: i32,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 pub struct SprDbEntry {
-    pub name: String,
     index: u16,
+    pub name: String,
 }
